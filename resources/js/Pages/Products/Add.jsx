337c7300f899e25aac5e66f9fba4953260df80eb
ERaufi <79995615+ERaufi@users.noreply.github.com>
2024-06-12 23:59:43 +0300

    Add Products to database

---
+++ resources/js/Pages/Products/Add.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Head, useForm } from '@inertiajs/react'
+import Authenticated from '@/Layouts/AuthenticatedLayout'
+
+export default function Add({ auth }) {
+    const { data, setData, post } = useForm({
+        name: '',
+        buyingPrice: '',
+        sellingPrice: '',
+    })
+
+    function submit(e) {
+        e.preventDefault()
+        post(route('products.create'))
+    }
+
+    return (
+        <Authenticated user={auth.user} header={<h2>Add New Product</h2>}>
+            <Head title='Add Products' />
+            <form onSubmit={submit}>
+                <label htmlFor="name">name</label>
+                <input
+                    className='form-control'
+                    type="text"
+                    value={data.name}
+                    onChange={e => setData('name', e.target.value)}
+                />
+                <label htmlFor="buyingPrice">buyingPrice</label>
+                <input
+                    className='form-control'
+                    type="number"
+                    value={data.buyingPrice}
+                    onChange={e => setData('buyingPrice', e.target.value)}
+                />
+                <label htmlFor="sellingPrice">sellingPrice</label>
+                <input
+                    className='form-control'
+                    type="number"
+                    value={data.sellingPrice}
+                    onChange={e => setData('sellingPrice', e.target.value)}
+                />
+                <button
+                    className='btn btn-success'
+                    type="submit">
+                    Save
+                </button>
+            </form>
+        </Authenticated>
+    )
+}
